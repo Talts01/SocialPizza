6749311dbe937d878b4f468e4fa5ec9dd2d02974
Luigi Niso <luigi.niso@edu.unito.it>
2026-01-24 18:03:47 +0100

    Delete Backend directory
    Mi urta la B maiuscola

--- Backend/src/main/java/com/socialpizza/backend/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.socialpizza.backend.repository;
-
-import com.socialpizza.backend.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {}
--- Backend/src/main/java/com/socialpizza/backend/repository/CityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.socialpizza.backend.repository;
-
-import com.socialpizza.backend.entity.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CityRepository extends JpaRepository<City, Long> {
-}
