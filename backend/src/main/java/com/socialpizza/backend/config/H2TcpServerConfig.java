6336d344fc0720c681d21116f51adb1ff518b273
nisossssss <luigi.niso@edu.unito.it>
2026-01-31 18:40:00 +0100

    Setup per VSCode Extension

---
+++ backend/src/main/java/com/socialpizza/backend/config/H2TcpServerConfig.java
@@ -0,0 +1,21 @@
+package com.socialpizza.backend.config;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+public class H2TcpServerConfig {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2TcpServer() throws SQLException {
+        return Server.createTcpServer(
+                "-tcp",
+                "-tcpAllowOthers",
+                "-tcpPort",
+            "9092"
+        );
+    }
+}
